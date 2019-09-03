17c66828aa8f22360313f634dfb0b4d0c238a183
Panashe Desktop VMWare <panashe@outlook.com>
2019-09-03 23:14:08 +1200

    Refactor code - remove unnecessary imports

--- exo/__init__.py
+++ exo/__init__.py
@@ -1,9 +1,1 @@
-from .api import *
-from .auth import *
-from .basemanager import *
-from .settings import *
-from .exceptions import *
-from .manager import *
-from .utils import *
-
 __version__ = "0.1"
